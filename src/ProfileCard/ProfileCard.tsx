9ca25ef4743e079c4fe1a2988ffe8453f8cd5a18
viveksingh-01 <viveksingh9301@gmail.com>
2020-09-04 21:45:50 +0530

    Update ProfileCard component and css

--- src/ProfileCard/ProfileCard.tsx
+++ src/ProfileCard/ProfileCard.tsx
@@ -2,7 +2,12 @@ import React from 'react';
 import './ProfileCard.css';
 
 const ProfileCard = () => {
-  return <div className="container-card"></div>;
+  return (
+    <div className="card">
+      <div className="card__user-info"></div>
+      <div className="card__user-stats"></div>
+    </div>
+  );
 };
 
 export default ProfileCard;
