7d0a91496f7ed7ecf11a7815b8de1cd5d4bf32c9
viveksingh-01 <viveksingh9301@gmail.com>
2020-09-04 21:20:02 +0530

    Add profile-card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import ProfileCard from './ProfileCard/ProfileCard';
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
+    <section className="profile-card__container">
+      <ProfileCard />
+    </section>
   );
 };
 
---
+++ src/ProfileCard/ProfileCard.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './ProfileCard.css';
+
+const ProfileCard = () => {
+  return <div className="container-card"></div>;
+};
+
+export default ProfileCard;
