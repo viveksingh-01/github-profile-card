aff33ada25b5c1e2355da4853fafc257a2365cf9
viveksingh-01 <viveksingh9301@gmail.com>
2020-09-05 17:53:26 +0530

    Fetch user-data from Github

--- src/ProfileCard/ProfileCard.tsx
+++ src/ProfileCard/ProfileCard.tsx
@@ -1,7 +1,52 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import './ProfileCard.css';
 
 const ProfileCard = () => {
+  const [userInfo, setUserInfo] = useState();
+  useEffect(() => {
+    const username = 'buckyroberts';
+    fetchUserData(username);
+  }, []);
+
+  const fetchUserData = async (username: string): Promise<any> => {
+    const URL = 'https://api.github.com/users';
+    try {
+      const {
+        data: {
+          name,
+          location,
+          bio,
+          company,
+          public_repos,
+          followers,
+          following,
+          avatar_url,
+          html_url,
+          repos_url,
+          followers_url,
+          following_url
+        }
+      } = await axios.get(`${URL}/${username}`);
+      setUserInfo({
+        name,
+        location,
+        bio,
+        company,
+        public_repos,
+        followers,
+        following,
+        avatar_url,
+        html_url,
+        repos_url,
+        followers_url,
+        following_url
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="card">
       <div className="card__user-info"></div>
