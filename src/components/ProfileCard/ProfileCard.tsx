121471c4b06338b3f50582dbe94607b101f9743b
viveksingh-01 <viveksingh9301@gmail.com>
2020-09-07 12:53:14 +0530

    Add username validation check before fetching data

--- src/components/ProfileCard/ProfileCard.tsx
+++ src/components/ProfileCard/ProfileCard.tsx
@@ -3,40 +3,44 @@ import React, { Fragment, useEffect, useState } from 'react';
 import { SearchBox, UserInfo, UserStats } from '..';
 import './ProfileCard.css';
 
+const GITHUB_API_URL = 'https://api.github.com/users';
+
 const ProfileCard = () => {
   const [userInfo, setUserInfo] = useState();
   const [userStats, setUserStats] = useState();
   const [error, setError] = useState(false);
+
   useEffect(() => {
     const username = 'viveksingh-01';
     fetchUserData(username);
   }, []);
 
   const fetchUserData = async (username: string): Promise<any> => {
-    const URL = 'https://api.github.com/users';
-    try {
-      const {
-        data: { name, login, location, bio, company, public_repos, followers, following, avatar_url, html_url }
-      } = await axios.get(`${URL}/${username}`);
-      setUserInfo({
-        name,
-        location,
-        bio,
-        company,
-        avatar_url,
-        html_url,
-        login
-      });
-      setUserStats({
-        public_repos,
-        followers,
-        following,
-        html_url
-      });
-      setError(false);
-    } catch (err) {
-      setError(true);
-      console.error(err);
+    if (username) {
+      try {
+        const {
+          data: { name, login, location, bio, company, public_repos, followers, following, avatar_url, html_url }
+        } = await axios.get(`${GITHUB_API_URL}/${username}`);
+        setUserInfo({
+          name,
+          location,
+          bio,
+          company,
+          avatar_url,
+          html_url,
+          login
+        });
+        setUserStats({
+          public_repos,
+          followers,
+          following,
+          html_url
+        });
+        setError(false);
+      } catch (err) {
+        setError(true);
+        console.error(err);
+      }
     }
   };
 
